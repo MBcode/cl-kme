5f8469b8a99918f7be4d3ec6835f4e17f675bd1c
mike b <mike.bobak+git@gmail.com>
2013-10-03 15:41:12 -0700

    still somethings2look at

--- src/sys.lisp
+++ src/sys.lisp
@@ -167,7 +167,7 @@
 ;ml/micmac@                 pd/stmx@                   sw/semantic-wikipedia@
 ;ml/ml-progs@               sw/SWCLOS@                 sw/sicl@
 ;ml/mulm@                   sw/SWCLOS2@                sw/twinql_0.3.1@
-;ml/pulcinella@             sw/SWCLOSforILC2005@
+;ml/pulcinella@             sw/SWCLOSforILC2005@       sw/reasoner-3.2@
 ;pd/Common-Lisp-Actors@     sw/cl-4store@              sw/cl-ace@
-;cl-graph graph-utils epigraph ;vivace-graph-v2
+;cl-graph graph-utils epigraph ;vivace-graph-v2           cl-nlp
 ;clpython  rcl rclg rclmath
