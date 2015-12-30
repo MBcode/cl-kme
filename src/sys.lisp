a2dc235e23ca39555d2a08ebfd8d529cc9c14eee
MBcode <bobak@balisp.org>
2015-12-30 12:05:44 -0800

    springer-lisp-books

--- src/sys.lisp
+++ src/sys.lisp
@@ -214,6 +214,8 @@
 
 ;pd: http://link.springer.com/book/10.1007/BFb0024148  Parallel Lisp: Languages and Systems
 
+; @RainerJoswig  #lisp books: the mentioned downloadable Lisp books published by Springer, listed: http://lispm.de/springer-lisp-books …
+
 ;Paul Khuong @pkhuong  .@GaborMelis asked me about http://www.pvk.ca/Blog/Lisp/Pipes/introducing_pipes.html … and I had to rediscover the domain specific type system I wrote 4 years ago :|
  
 ;m/Maxima-CAS        m/gnoem             m/mma               m/rclmath
